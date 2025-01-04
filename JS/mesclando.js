aa2c59255736d924a6e37c7b4de9ca6e7d317dab
DaniloSantanleite <danilo.baumann@icloud.com>
2025-01-04 13:06:24 -0300

    Mesclando o html e js

---
+++ JS/mesclando.js
@@ -0,0 +1,7 @@
+const input = document.querySelector("input")
+const select = document.querySelector("select")
+const button = document.querySelector("button")
+
+input.addEventListener("focus", () =>{
+    console.log("dei um foco")
+})
