c9a65817425b05898e198fe358118e936edf7ed5
DaniloSantanleite <danilo.baumann@icloud.com>
2025-01-06 12:27:54 -0300

    Modo Dark - JS

--- BackGroundColor/index.js
+++ BackGroundColor/index.js
@@ -1,5 +1,7 @@
 const element = document.getElementById("Clique")
 const all = document.querySelector("body")
+const square = document.getElementById("quadrado");
+
 
 changeBackGround = () => {
      if (all.style.backgroundColor === "rgb(255, 255, 255)" || all.style.backgroundColor === "") {
@@ -8,5 +10,18 @@ changeBackGround = () => {
       
       all.style.backgroundColor = "rgb(255, 255, 255)";
     }
-  };
+  }
+
+  const changeBackGroundSquare = () => {
+    if (all.style.backgroundColor === "rgb(255, 255, 255)" || all.style.backgroundColor === "") {
+      square.style.backgroundColor = "black"  
+    } else {
+      square.style.backgroundColor = "rgb(255, 255, 255)"  
+  }
+}
+  
 element.addEventListener("click",changeBackGround)
+element.addEventListener("click",changeBackGroundSquare)
+
+
+ 
